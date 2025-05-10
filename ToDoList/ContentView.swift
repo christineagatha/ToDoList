079af4f777e2bf502badc1a4be5a2fb15a16a951
christineagatha <christinejiang09@gmail.com>
2025-05-09 23:07:59 -0400

    was having issues with delete (simulator kept stopping at breakpoint) but its good now

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -9,14 +9,16 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @State private var showNewTask = false
     @Query var toDos: [ToDoItem]
+    @Environment(\.modelContext) var modelContext
+    @State private var showNewTask = false
     
     var body: some View {
         VStack {
             HStack {
                 Text("To Do List")
-                    .font(.system(size: 40))
+//                    .font(.system(size: 40))
+                    .font(.title)
                     .fontWeight(.black)
                 Spacer()
                 Button {
@@ -39,12 +41,19 @@ struct ContentView: View {
                         Text(toDoItem.title)
                     }
                 }
+                .onDelete(perform: deleteToDo)
             }
             .listStyle(.plain)
         }
         if showNewTask {
             NewToDoView(showNewTask: $showNewTask, toDoItem: ToDoItem(title: "", isImportant: false))
 
+        }
+    }
+    func deleteToDo(at offsets: IndexSet) {
+        for offset in offsets {
+            let toDoItem = toDos[offset]
+            modelContext.delete(toDoItem)
         }
     }
 }
